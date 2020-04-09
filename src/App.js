dc7e0fd8dbbfd15107de941ee75bddb0ae5e729c
DanSimonson <dansimonson@mariposaweb.net>
2020-04-08 23:05:55 -0600

    added blog page and changed height of about section in items page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.scss';
-import Portfolio from './Views/Portfolio';
-import Footer from './Components/Footer/Footer'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.scss";
+import Portfolio from "./Views/Portfolio";
+import Blog from "./Views/Blog";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div> 
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Portfolio} />
-      </Switch>
-    </BrowserRouter>
-    <Footer/>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Portfolio} />
+          <Route exact path="/Blog" component={Blog} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
