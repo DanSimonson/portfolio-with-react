2cc8d1469da3dbe5bbb7a6386cddd37378114b9b
DanSimonson <dansimonson@mariposaweb.net>
2020-04-10 07:50:40 -0600

    put data in cards that are fully responsive in css grid

--- src/Views/Blog.js
+++ src/Views/Blog.js
@@ -1,25 +1,19 @@
 import React from "react";
 import "./Blog.scss";
 import BlogList from "../Components/BlogList/BlogList";
+import blogPosts from "../Data/PostData.json";
 
 const Blog = () => {
+  const cardsArray = blogPosts.map((blogPost) => (
+    <div className="blog-cards" key={blogPost.id}>
+      <p>{blogPost.title}</p>
+    </div>
+  ));
+
+  //return <div>{cardsArray}</div>;
   return (
     <div className="blog-wrapper">
-      <section className="basic-grid">
-        <BlogList />
-        {/*<div className="blog-cards">1</div>
-        <div className="blog-cards">2</div>
-        <div className="blog-cards">3</div>
-        <div className="blog-cards">4</div>
-        <div className="blog-cards">5</div>
-        <div className="blog-cards">6</div>
-        <div className="blog-cards">7</div>
-        <div className="blog-cards">8</div>
-        <div className="blog-cards">9</div>
-        <div className="blog-cards">10</div>
-        <div className="blog-cards">11</div>
-        <div className="blog-cards">12</div>*/}
-      </section>
+      <section className="basic-grid">{cardsArray}</section>
     </div>
   );
 };
