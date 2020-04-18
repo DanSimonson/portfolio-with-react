a9d81449c4bbd11352952bfcead3cd67c73db74b
DanSimonson <dansimonson@mariposaweb.net>
2020-04-18 15:28:16 -0600

    added wrapper

---
+++ src/Views/NewBlogPost.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./NewBlogPost.scss";
+import { connect } from "react-redux";
+import { createStore } from "redux";
+import RootReducer from "../Reducers/RootReducer";
+import { getFirestore } from "../WrapFirestore";
+import firebase from "./Firestore";
+
+function NewBlogPost(props) {
+  const db = firebase.firestore();
+  let temp = getFireData();
+  console.log("firestoreData: ", temp);
+  //console.log("props: ", props);
+  const store = createStore(RootReducer);
+  //console.log("store: ", store.getState());
+
+  store.subscribe(() => {
+    console.log("subscription: ", store.getState());
+  });
+
+  const handleClick = () => {
+    //console.log("in handleClick props", props);
+    //props.handleClick();
+    store.dispatch({ type: "ADD_BLOGPOST" });
+  };
+
+  return (
+    <div>
+      <h1>Create Blog Post</h1>
+      <button className="Btn" onClick={handleClick}>
+        Create
+      </button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    BlogPosts: state.BlogPosts,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleClick: (dispatch) => NewBlogPost("ADD_BLOGPOST"),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewBlogPost);
+//export default connect(mapStateToProps)(Blog);
